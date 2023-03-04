ba6677fe947e94937240bdff4532b09215203f8f
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-04 15:40:50 +0530

    Update and rename fg.js to ff.js

--- ff.js
+++ ff.js
@@ -1,7 +1,7 @@
 const options = { method: "GET"};
 
 const myFunc = async () => {
-  const a = await fetch("http://165.232.190.5:1900/flag", options)
+  const a = fetch("http://165.232.190.5:1900/flag", options)
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err)) => console.log(err));
